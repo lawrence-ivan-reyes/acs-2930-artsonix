0675a071afd0c444cc30c7a08bf143cc0d7bccf6
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-13 02:06:21 -0800

    updated gitignore and added unit tests

---
+++ test_app.py
@@ -0,0 +1,33 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'index' in response.data
+
+def test_about(client):
+    response = client.get('/about')
+    assert response.status_code == 200
+    assert b'about' in response.data
+
+def test_credits(client):
+    response = client.get('/credits')
+    assert response.status_code == 200
+    assert b'credits' in response.data
+
+def test_results(client):
+    response = client.get('/results')
+    assert response.status_code == 200
+    assert b'results' in response.data
+
+def test_error(client):
+    response = client.get('/error')
+    assert response.status_code == 200
+    assert b'error' in response.data
