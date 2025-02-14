b1506caf6da3d381fca95e7749b0d9a44ffae13e
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-14 01:55:31 -0800

    updated filter

--- josh_spotify.py
+++ josh_spotify.py
@@ -163,6 +163,10 @@ def format_results(items, search_type):
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/results', methods=['GET'])
 def results():
     """Fetches Spotify results, applies NSFW filtering, and renders an HTML page."""
